78c57797a4ee01edb959545ab1402c57de6f48a8
cow-98 <sojh8908@naver.com>
2023-08-07 15:42:34 +0900

    안드로이드 앱개발 1주차

--- testtest/app/src/main/java/com/example/testtest/Zombie.kt
+++ testtest/app/src/main/java/com/example/testtest/Zombie.kt
@@ -1,22 +1,23 @@
 package com.example.testtest
 
-class Zombie {
-    var name:String = ""
-    var color:String = ""
-    var height:Double = 0.0
-    var hp:Int = 0
-    var damage:Int = 0
+class Zombie : Monster {
+    var name:String
+    var color:String
+    var height:Double
+    var hp:Int
+    var damage:Int
 
     constructor(_name:String, _color:String, _height:Double, _hp:Int, _damage:Int) {
-        println("${name}좀비 생성")
+//        println("${name}좀비 생성")
         name = _name
         color = _color
         height = _height
         hp = _hp
         damage = _damage
+        println("${name}좀비 생성")
     }
 
-    fun attack() {
+    override fun attack() {
         println("물어뜯기 공격!")
     }
 
