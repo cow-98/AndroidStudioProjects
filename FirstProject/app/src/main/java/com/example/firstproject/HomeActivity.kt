5b85c0e0ff7b832c3dbfb00cc1c03b2b388702c2
junseon <sojh8908@naver.com>
2023-08-11 11:34:26 +0900

    안드로이드 개인과제 Lv5 fin

--- FirstProject/app/src/main/java/com/example/firstproject/HomeActivity.kt
+++ FirstProject/app/src/main/java/com/example/firstproject/HomeActivity.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 
 class HomeActivity  : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +20,18 @@ class HomeActivity  : AppCompatActivity() {
 
         val btn4 = findViewById<Button>(R.id.button4)
         btn4.setOnClickListener{home(it)}
+
+        val img = findViewById<ImageView>(R.id.imageView6)
+
+        val imgview =when ((1..5).random()) {
+            1 -> R.drawable.snp3
+            2 -> R.drawable.snp4
+            3 -> R.drawable.snp5
+            4 -> R.drawable.snp6
+            5 -> R.drawable.snp7
+            else -> R.drawable.snp3
+        }
+        img.setImageDrawable(ResourcesCompat.getDrawable(resources, imgview, null))
     }
 
     fun home(v:View) {
