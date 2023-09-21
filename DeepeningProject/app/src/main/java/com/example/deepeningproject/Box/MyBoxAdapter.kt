7df6f5212030f27dc7e66c02ccb48f4b72095003
junseon <sojh8908@naver.com>
2023-09-22 02:30:48 +0900

    서치 프래그먼트 데이터 불러오기

---
+++ DeepeningProject/app/src/main/java/com/example/deepeningproject/Box/MyBoxAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.deepeningproject.Box
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.deepeningproject.Search.SearchFragmentAdapter
+
+//class MyBoxAdapter(var mContext: Context): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+//
+//
+//
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyBoxAdapter.ItemViewHolder {
+//
+//    }
+//
+//
+//
+//    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+//
+//
+//    }
+//
+//
+//    override fun getItemCount(): Int {
+//
+//    }
+//
+//
+//
+//
+//
+//
+//
+//
+//    inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+//
+//    }
+//}
---
+++ DeepeningProject/app/src/main/java/com/example/deepeningproject/SearchItem.kt
@@ -0,0 +1,9 @@
+package com.example.deepeningproject
+
+
+data class SearchItem(
+    var title: String,
+    var dateTime: String,
+    var url: String,
+    var islike: Boolean = false
+)
